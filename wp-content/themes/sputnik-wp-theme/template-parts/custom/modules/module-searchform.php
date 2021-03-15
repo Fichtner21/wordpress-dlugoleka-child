2907e780125992ba9f05cc15dc74cd54be9bb7f0
przemyslaw.drozniak <biuro@interakcjo.pl>
2021-03-15 10:47:20 +0100

    Some change

--- wp-content/themes/sputnik-wp-theme/template-parts/custom/modules/module-searchform.php
+++ wp-content/themes/sputnik-wp-theme/template-parts/custom/modules/module-searchform.php
@@ -2,7 +2,7 @@
 <?php $ID = wp_unique_id(); ?>
 
 <form id="searchform" method="get" action="<?= esc_url( home_url( '/' ) ); ?>">
-    <label for="d-search-form-input-<?= $ID; ?>" class="screen-reader-text"><?= __('Szukaj','sputnik-wp-theme'); ?></label>
+    <label for="d-search-form-input-<?= $ID; ?>" class="screen-reader-text"><?= __('Szukaj','sputnik-wp-theme'); ?>-<?= $ID; ?></label>
 
     <input type="text" class="search-field" id='d-search-form-input-<?= $ID; ?>' name="s" placeholder="<?= __('Szukaj...','sputnik-wp-theme'); ?>" value="<?= get_search_query(); ?>">
 
