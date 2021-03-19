430a15be9a834287f9c1e63f50609eaab542c852
Fichtner21 <ernest.fichtner@gmail.com>
2021-03-19 15:16:30 +0100

    Dodane module-map

--- wp-content/themes/sputnik-wp-theme/template-parts/content-map.php
+++ wp-content/themes/sputnik-wp-theme/template-parts/content-map.php
@@ -5,6 +5,13 @@ Template Name: Mapa interaktywna
 */
 
 get_header();
+?>
+<div class="container">
+<?php
+  require CUSTOM_PARTS . '/modules/module-breadcrumbs.php'; 
+?>
+</div>
+<?php
 
 require CUSTOM_PARTS . '/modules/module-map.php'; 
 
