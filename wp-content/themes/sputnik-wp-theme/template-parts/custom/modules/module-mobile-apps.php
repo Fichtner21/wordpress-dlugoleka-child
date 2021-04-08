6334f2c1097262af838fa08278a2d31808266e66
przemyslaw.drozniak <biuro@interakcjo.pl>
2021-04-08 10:13:55 +0200

    Literówka

--- wp-content/themes/sputnik-wp-theme/template-parts/custom/modules/module-mobile-apps.php
+++ wp-content/themes/sputnik-wp-theme/template-parts/custom/modules/module-mobile-apps.php
@@ -2,7 +2,7 @@
     <div class="apps">
         <img class="apps__phones" src='<?= get_template_directory_uri(); ?>/dist/public/images/app-phones.png' alt='Dwa telefony komórkowe'>
 
-        <h2 class="apps__title"><?= __('Pobierz Aplikację mobilna','sputnik-wp-theme'); ?></h2>
+        <h2 class="apps__title"><?= __('Pobierz Aplikację mobilną','sputnik-wp-theme'); ?></h2>
         <p class="apps__text"><?= !empty(get_option('apps_text')) ? get_option('apps_text') : null; ?></p>
 
         <div class="apps__buttons">
