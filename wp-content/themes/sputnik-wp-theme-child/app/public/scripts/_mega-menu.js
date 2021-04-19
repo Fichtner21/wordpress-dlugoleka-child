39d5e2c79660ca6aee0e151371e015109ad33f5c
przemyslaw.drozniak <biuro@interakcjo.pl>
2021-04-19 16:01:47 +0200

    background color menu

--- wp-content/themes/sputnik-wp-theme-child/app/public/scripts/_mega-menu.js
+++ wp-content/themes/sputnik-wp-theme-child/app/public/scripts/_mega-menu.js
@@ -13,7 +13,7 @@ if (jQuery) {
         e.preventDefault();
 
         const $megaMenu = $(this).next();
-        const $allSubmenus = $(".sub-menu");        
+        const $allSubmenus = $(".sub-menu");
 
         $allSubmenus.removeClass("focus");
 
@@ -29,7 +29,7 @@ if (jQuery) {
           $(this).attr("aria-expanded", "true");
           $megaMenu.addClass("focus");
         }
-      }            
+      }
 
       $topLevelItems.find("a:first").on("click", toggleMegaMenu);
 
@@ -47,14 +47,14 @@ if (jQuery) {
             $(this).attr("aria-expanded", "true");
           }
         }
-      });       
-     
-      $(document).click(function(event){     
-        const myTarget = $(".nav-menu");       
-        if(!myTarget.is(event.target) && myTarget.has(event.target).length === 0){
-          $('.focus').hide();
+      });
+
+      $(document).click(function (event) {
+        const myTarget = $(".nav-menu");
+        if (!myTarget.is(event.target) && myTarget.has(event.target).length === 0) {
+          $(".focus").hide();
         } else {
-          $('.focus').show();
+          $(".focus").show();
         }
       });
     });
